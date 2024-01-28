8ad26b14ef29cb3a923a38584394efac96b6ee03
Vikassingh927 <157972945+Vikassingh927@users.noreply.github.com>
2024-01-28 20:22:52 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,36 @@
+let num1 = 20
+let num2 = 10
+console.log("Sum of the numbers is: ", num1 + num2);
+console.log("Difference of the numbers: ", num1 - num2);
+
+let num3 = 30
+let num4 = 10
+console.log("Multiplication of the numbers is: ", num3 * num4);
+console.log("Quotient of the numbers is: ", num3 / num4);
+
+let num5 = 40
+let num6 = 10
+console.log("Equality :", num1 == num2);
+console.log("Strict Equality: ", num1 === num2);
+console.log("Inequality: ", num1 != num2);
+
+let num7 = 50
+let num8 = 10
+console.log("number 7 is greater than number 8 : ", num7 > num8);
+console.log("number 7 is less than 8 : ", num7 < num8);
+
+let isSunny = true
+let isWarm = false
+console.log(" sunny and warm ", isSunny && isWarm);
+console.log(" sunny or warm ", isSunny || isWarm);
+
+let isRainy = true
+console.log(" not raining ", !isRainy);
+
+let counter = 10
+console.log("Increment of value: ", ++counter);
+console.log("Decrement of value: ", --counter);
+
+let total = 9
+total += 1;
+console.log("updated value : ", total);
